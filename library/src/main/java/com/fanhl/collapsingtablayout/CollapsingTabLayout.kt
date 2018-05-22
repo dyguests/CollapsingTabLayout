c8fe73775e0c3994b7217d9119c932a843bab51b
fanhl <fanhonglin@klicen.com>
2018-05-22 10:58:14 +0800

    CollapsingTabLayout

---
+++ library/src/main/java/com/fanhl/collapsingtablayout/CollapsingTabLayout.kt
@@ -0,0 +1,21 @@
+package com.fanhl.collapsingtablayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class CollapsingTabLayout : ViewGroup {
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    init {
+
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
+}
---
+++ library/src/main/java/com/fanhl/collapsingtablayout/DummyTabLayout.java
@@ -0,0 +1,15 @@
+package com.fanhl.collapsingtablayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TableLayout;
+
+public class DummyTabLayout extends TableLayout {
+    public DummyTabLayout(Context context) {
+        super(context);
+    }
+
+    public DummyTabLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
