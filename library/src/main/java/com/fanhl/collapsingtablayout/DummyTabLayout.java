812bec12881bcd74100c7fde425efd975c39d315
fanhl <fanhonglin@klicen.com>
2018-05-22 11:15:35 +0800

    xml

--- library/src/main/java/com/fanhl/collapsingtablayout/DummyTabLayout.java
+++ library/src/main/java/com/fanhl/collapsingtablayout/DummyTabLayout.java
@@ -1,15 +1,20 @@
 package com.fanhl.collapsingtablayout;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
 import android.util.AttributeSet;
-import android.widget.TableLayout;
 
-public class DummyTabLayout extends TableLayout {
+public class DummyTabLayout extends TabLayout {
+
     public DummyTabLayout(Context context) {
         super(context);
     }
 
     public DummyTabLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public DummyTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
     }
 }
