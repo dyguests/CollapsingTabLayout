20ed59f3b58203e76eeb4b4903650c26232ce4ef
fanhl <fanhonglin@klicen.com>
2018-05-22 15:02:41 +0800

    可展开成多行的TabLayout

--- library/src/main/java/com/fanhl/collapsingtablayout/CollapsingTabLayout.kt
+++ library/src/main/java/com/fanhl/collapsingtablayout/CollapsingTabLayout.kt
@@ -3,9 +3,15 @@ package com.fanhl.collapsingtablayout
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.ViewGroup
 
+/**
+ * 可展开成多行的TabLayout
+ *
+ * @author fanhl
+ */
 class CollapsingTabLayout : ViewGroup {
     constructor(context: Context) : super(context) {}
 
@@ -17,10 +23,29 @@ class CollapsingTabLayout : ViewGroup {
 
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+    }
+
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
 
     }
 
+    /**
+     * 这个方法应该没什么用
+     */
+    override fun dispatchDraw(canvas: Canvas?) {
+        super.dispatchDraw(canvas)
+//        val textPaint = TextPaint()
+//        textPaint.color = Color.BLACK
+//        textPaint.textSize = 20f
+//        canvas?.drawText("测试", 10f, 10f, textPaint)
+    }
+
+    /**
+     * 这个也没用
+     */
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
     }
